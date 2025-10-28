65f9a752cbddea1f72414bfaccaf80d243881b03
patxifer22 <patxifer22@gmail.com>
2025-10-28 16:40:31 +0100

    update

--- MPO/PrimerProyectoMPO/src/Main.java
+++ MPO/PrimerProyectoMPO/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("PROYECTO METODOS");
+        System.out.println("Vamos a realizar llamadas a metodos");
+        saludar("Patxi");
+        saludar("Laura");
+        saludar("Jorge");
     }
-}
+
+    public  static void saludar(String nombre){
+            System.out.println("Hola "+nombre);
+            System.out.println("Encantado de saludarte, vamos a practicar");
+            System.out.println("Vamos a practicar el llamado de metodos");
+            System.out.println("Animo, no decaigas en tu tarea");
+        }
+    }
